ca4427744bef57c2ba49d24de2ca9c4e6a84ec74
Sohee KWON <soheekwon@naver.com>
2020-11-18 03:52:42 +0900

    create clock

---
+++ clock.js
@@ -0,0 +1,61 @@
+const aHour = document.querySelector(".a-hour"),
+    aMinute = document.querySelector(".a-minute"),
+    aSecond = document.querySelector(".a-second"),
+    dTime = document.querySelector(".d-time"),
+    dBtn = document.querySelector(".d-button");
+
+let hour24 = false;
+
+function fillZero(num) {
+    num = num + "";
+    if (num.length < 2) {
+        return "0" + num;
+    } else {
+        return num;
+    }
+}
+
+function changeTime() {
+    if (hour24) {
+        hour24 = false;
+    }
+    else {
+        hour24 = true;
+        dBtn.innerHTML = "24H";
+    }
+}
+
+function drawClock() {
+    const now = new Date();
+
+    const hour = now.getHours(),
+        minute = now.getMinutes(),
+        second = now.getSeconds();
+
+    const hourDegree = (hour + minute / 60) * (360 / 12) + 90,
+        minuteDegree = (minute + second / 60) * (360 / 60) + 90,
+        secondDegree = second * (360 / 60) + 90;
+    
+    aHour.style.transform = `rotate(${hourDegree}deg)`;
+    aMinute.style.transform = `rotate(${minuteDegree}deg)`;
+    aSecond.style.transform = `rotate(${secondDegree}deg)`
+    
+    if (!hour24) {
+        if (hour >= 0 && hour <= 11) {
+            if (hour == 0) hour = 12;
+            dBtn.innerHTML = "AM";
+        } else {
+            if (hour >= 13) hour -= 12;
+            dBtn.innerHTML = "PM";
+        }
+    }
+
+    dTime.innerHTML = `${fillZero(hour)} : ${fillZero(minute)} : ${fillZero(second)}`;
+}
+
+function init() {
+    setInterval(drawClock, 1000);
+    dBtn.addEventListener("click", changeTime);
+}
+
+init();
